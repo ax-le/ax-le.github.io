c20e59c320999acceb75db3c319953a53bb346b8
Axel Marmoret <axel.marmoret@inria.fr>
2022-03-08 19:07:22 +0100

    Rollback because github pages does not support PHP

--- about_me.php
+++
@@ -1,6 +0,0 @@
-<div class="about">
-  <h3>About Me</h3>
-  Hello! My name is Axel Marmoret, I'm 25, and I'm a PhD Student at IRISA, Rennes, in France.<br>
-  I work on Machine Learning and Optimisation techniques for retrieving musical structure, based on its audio form.<br>
-  I'm also a musician (mainly drummer, newly bassist!), and passionate about music.
-</div>
--- footer.php
+++
@@ -1,5 +0,0 @@
-<footer>
-  <div class="copyright">
-	<p>For the theme: Copyright (c) 2020 by Naomi Bastian Weatherford <a target="_blank" class="inner-link" href="https://codepen.io/astronaomical/pen/KexYgb">(https://codepen.io/astronaomical/pen/KexYgb)</a></p>
-  </div>
-</footer>
